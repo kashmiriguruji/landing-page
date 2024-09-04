b0f7b68576e834063252c7d3d5f58e2d9f1f85bf
Guruji <kashmiriguruji.com@gmail.com>
2024-09-04 11:21:02 +0000

    change layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,82 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          We're building something amazing. Stay tuned for the launch.
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="http://paramarsh.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By <strong>Paramarsh Informatics</strong>
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1]  text-center place-items-center before:absolute  before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h1 className=" lg:text-6xl text-2xl font-bold uppercase text-black mb-8 ">
-          Kashmiriguruji.com
-        </h1>
-        <p className="text-black">Coming soon</p>
-      </div>
-
-      <div className="mb-32 grid text-center  lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
-        <a
-          href="mailto:pawanjipandita@gmail.com"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Email{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          pawanjipandita@gmail.com
-          </p>
-        </a>
-
-        <a
-          href="tel:+918700398329"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Phone{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          8700398329 / 9899375987
-          </p>
-        </a>
-
-        <a
-          href="https://www.google.com/maps/place/Pocket-C,+Mayur+Vihar+Phase+II,+Mayur+Vihar+Phase+I,+Delhi,+110091/@28.6187064,77.3058714,18z/data=!3m1!4b1!4m6!3m5!1s0x390ce4b9b4bed5a7:0xc9551ff6d3381165!8m2!3d28.6182925!4d77.3073133!16s%2Fg%2F12hmb_jxk?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Address{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2> 
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          14 C, Pocket – C Mayur Vihar, Phase II Delhi - 110091
-          </p>
-        </a>
-      </div>
+    <main>
+      <h1>Coming Soon</h1>
     </main>
   );
 }
