180e7d3e6a9f5fe69cb29ea4371f4d645c8c2123
Guruji <kashmiriguruji.com@gmail.com>
2024-09-05 09:11:46 +0000

    added image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Coming Soon</h1>
+      <section>
+        <h1>kashmiriguruji.com</h1>
+        <div className="mid">
+          <img src="/images.jpg" alt="" />
+          <h2>Contact</h2>
+          <h3>Pt. Pawan Ji Pandita</h3>
+          <p>Mobile: 8700398329 / 9899375987</p>
+          <p>Email: pawanjipandita@gmail.com</p>
+          <p>
+            Address: 14 C, Pocket - C, Mayus Viahr, Phase II, New Delhi - 110091
+          </p>
+        </div>
+      </section>
+      <footer>
+        <p>Koshur Sanaskar Jagrati Mission Trust (Regd.)</p>
+      </footer>
     </main>
   );
 }
