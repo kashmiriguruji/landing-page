1483b91fb44290f0704eefef7f0df530af4381f3
Guruji <kashmiriguruji.com@gmail.com>
2024-09-09 10:04:45 +0000

    added link coming soon text

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,21 @@ export default function Home() {
     <main>
       <section>
         <h1>kashmiriguruji.com</h1>
+        <h2 className="subtext">Coming Soon ....</h2>
         <div className="mid">
           <img src="/images.jpg" alt="" />
           <h2>Contact</h2>
           <h3>Pt. Pawan Ji Pandita</h3>
-          <p>Mobile: 8700398329 / 9899375987</p>
-          <p>Email: pawanjipandita@gmail.com</p>
+          <p>
+            Mobile: <a href="tel:8700398329">8700398329</a> /{" "}
+            <a href="tel:9899375987">9899375987</a>
+          </p>
+          <p>
+            Email:{" "}
+            <a href="mailto:pawanjipandita@gmail.com">
+              pawanjipandita@gmail.com
+            </a>
+          </p>
           <p>
             Address: 14 C, Pocket - C, Mayur Vihar, Phase II, New Delhi - 110091
           </p>
